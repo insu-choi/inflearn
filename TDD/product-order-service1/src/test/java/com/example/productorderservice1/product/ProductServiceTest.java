7d767b8b94f94984f8a774f6a2b837f9db0a31cb
var <var@kakao.com>
2023-09-04 03:01:21 +0900

    TDD SpringBootTest

--- TDD/product-order-service1/src/test/java/com/example/productorderservice1/product/ProductServiceTest.java
+++ TDD/product-order-service1/src/test/java/com/example/productorderservice1/product/ProductServiceTest.java
@@ -1,40 +1,33 @@
 package com.example.productorderservice1.product;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@SpringBootTest
 public class ProductServiceTest {
+    @Autowired
     private ProductService productService;
+    @Autowired
     private ProductPort productPort;
 
+    @DisplayName("상품수정")
     @Test
     void 상품수정() {
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         final Long productId = 1L;
         final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-        productPort = new ProductPort() {
-            @Override
-            public void save(Product product) {
 
-            }
-
-            @Override
-            public Product getProduct(long productId) {
-                return product;
-            }
-
-            @Override
-            public String getProductName(long productId) {
-                return null;
-            }
-        };
-        productService = new ProductService(productPort);
         productService.updateProduct(productId, request);
-
-        assertThat(product.getName()).isEqualTo("상품 수정");
-        assertThat(product.getPrice()).isEqualTo(2000);
+        final ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        final GetProductResponse productResponse = response.getBody();
+        assertThat(productResponse.name()).isEqualTo("상품 수정");
+        assertThat(productResponse.price()).isEqualTo(2000);
     }
 
 }
