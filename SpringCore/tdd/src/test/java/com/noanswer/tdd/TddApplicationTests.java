45977c68e633975f52e9fc55e875c571506237e5
var <var@kakao.com>
2023-09-03 21:31:19 +0900

    SpringCore

---
+++ SpringCore/tdd/src/test/java/com/noanswer/tdd/TddApplicationTests.java
@@ -0,0 +1,26 @@
+package com.noanswer.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+
+        TestClass testClass = new TestClass();
+
+    }
+
+    @Test
+    void test1() {
+
+    }
+
+    @Test
+    void korean() {
+
+    }
+}
