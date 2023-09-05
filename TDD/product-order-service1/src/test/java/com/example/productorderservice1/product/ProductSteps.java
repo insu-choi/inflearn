ff5898f643d02361bb857a9b3f2ed8a191a1ce8b
var <var@kakao.com>
2023-09-05 23:58:22 +0900

    [TDD] 상품 주문 API 테스트 전환

--- TDD/product-order-service1/src/test/java/com/example/productorderservice1/product/ProductSteps.java
+++ TDD/product-order-service1/src/test/java/com/example/productorderservice1/product/ProductSteps.java
@@ -6,7 +6,7 @@ import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
 public class ProductSteps {
-    static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
+    public static ExtractableResponse<Response> 상품등록요청(AddProductRequest request) {
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
