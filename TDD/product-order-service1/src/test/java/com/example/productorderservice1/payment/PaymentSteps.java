b8c2089ff657ad5e1205f2b59b19af1c64783967
var <var@kakao.com>
2023-11-26 17:27:14 +0900

    [TDD] 주문 결제 Api 테스트로 전환

--- TDD/product-order-service1/src/test/java/com/example/productorderservice1/payment/PaymentSteps.java
+++ TDD/product-order-service1/src/test/java/com/example/productorderservice1/payment/PaymentSteps.java
@@ -1,9 +1,24 @@
 package com.example.productorderservice1.payment;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
 public class PaymentSteps {
     public static PaymenetRequest 주문결제요청_생성() {
         final Long orderId = 1L;
         final String cardNumber = "1234-1234-1234-1234";
         return new PaymenetRequest(orderId, cardNumber);
+    }
+
+    static ExtractableResponse<Response> 주문결제요청(PaymenetRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/payments")
+                .then().log().all()
+                .extract();
     }
 }
